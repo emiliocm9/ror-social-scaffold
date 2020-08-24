19e2df1d09e6a2aaca529a75c2a384d1130f0a12
brugobi <brgobbi@hotmail.com>
2020-08-24 16:37:55 -0400

    add unit tests to like model

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  
+  describe 'association' do
+    it{ is_expected.to belong_to(:user)}
+    it{ is_expected.to belong_to(:post)}
+  end
+
+  describe "validations" do
+    it{ should validate_uniqueness_of(:user_id).scoped_to(:post_id)}
+  end
+
+end
